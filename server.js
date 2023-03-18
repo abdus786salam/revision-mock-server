c7e5daede95db59e9a536dd9a3024adfd19be83e
Abdus Salam <abdus786salam@gmail.com>
2023-03-18 10:02:01 +0530

    update 6

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
 server.use(middlewares);
-server.use(cors())
+server.use(cors({
+    origin:"*"
+}))
 server.use(router);
 
 server.listen(port);
