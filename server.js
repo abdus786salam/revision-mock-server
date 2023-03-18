91ebd2ce93f2acb9b4eacf08b3ada5c6fc3b8408
Abdus Salam <abdus786salam@gmail.com>
2023-03-18 10:35:01 +0530

    update 8

--- server.js
+++ server.js
@@ -5,11 +5,6 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
 server.use(middlewares);
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
-    res.header('Access-Control-Allow-Headers', '*')
-    next()
-  })
 server.use(router);
 
 server.listen(port);
